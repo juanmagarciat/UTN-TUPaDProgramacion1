ccd2a6b90b9b856dd4498b79afa90d061d902c8b
juanmagarciat <garciajuanmartin258@gmail.com>
2025-08-24 20:38:14 -0300

    Create ej_complementarios.py

---
+++ ej_complementarios.py
@@ -0,0 +1,38 @@
+#1)
+numero1 = 10
+#2)
+numero2 = 10.5
+#3)
+suma = numero1 + numero2
+#4)
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print(f"SUMA: {suma} RESTA: {resta} DIVISIÓN: {division} MULTIPLICACIÓN: {multiplicacion}")
+#5)
+nombre = "Juan Martin"
+#6)
+precio = 912.18
+#7)
+descuento = 0.31
+#8)
+precio_final = precio * (1-descuento)
+#9)
+cadena = "Hola, que tal?"
+#10)
+longitud = len(cadena)
+#11)
+precio = int(912.18)
+#12)
+nombre = "Juan Martin"
+apellido = "Garcia"
+nombre_completo = f"{nombre} {apellido}"
+#13)
+edad = (19 + 5) - 10
+#14)
+altura = (1.80 * 4) / 3
+#15)
+nombre_mayus = "JUAN MARTIN"
+nombre_minus = nombre_mayus.lower()
+#16)
+nombre_titulo = nombre_minus.title()
