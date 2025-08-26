108577fc7318ef2e1989ca4348dc136a19b666bb
juanmagarciat <garciajuanmartin258@gmail.com>
2025-08-25 21:34:57 -0300

    Calculo el porcentaje de asistencia de Practica Hablada

--- UNIDAD 2 CONDICIONALES/ejercicio_condicionales.py
+++ UNIDAD 2 CONDICIONALES/ejercicio_condicionales.py
@@ -18,11 +18,23 @@ if mes_num not in range(1,13):
 dias_por_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 if dia_num < 1 or dia_num > dias_por_mes[mes_num-1]:
     print("ERROR: EL DIA NO CORRESPONDE A UNA FECHA CORRECTA")
-#SI EL ESTUDIANTE ES DE NIVEL INICIAL, INTERMEDIO O AVANZADO VERIFICAMOS SI SE TOMARON EXAMENES
+#SI EL USUARIO ES DE NIVEL INICIAL, INTERMEDIO O AVANZADO VERIFICAMOS SI SE TOMARON EXAMENES Y EL PORCENTAJE DE APROBADOS
 examen = ["Lunes", "Martes", "Miercoles"]
 if dia_semana in examen:
-    tomaron_examen = input("¿TOMARON EXAMEN? SI/NO") 
+    tomaron_examen = input("¿TOMARON EXAMEN? SI/NO: ") 
     if tomaron_examen == "Si" or tomaron_examen == "si":
-        aprobados = int(input("¿CUANTOS APROBARON? "))
-        desaprobados = int(input("¿CUANTOS DESAPROBARON? "))
+        aprobados_examen = int(input("¿CUANTOS APROBARON?:  "))
+        desaprobados_examen = int(input("¿CUANTOS DESAPROBARON?:  "))
+        total_examenes = aprobados_examen + desaprobados_examen
+        porcentaje_aprobados = (aprobados_examen/total_examenes) * 100
+        print(f"EL PORCENTAJE DE APROBADOS ES {porcentaje}%")
+#SI EL USUARIO ES DE PRÁCTICA HABLADA VERIFICAMOS EL PORCENTAJE DE ASISTENCIA
+if dia_semana == "Jueves":
+    porcentaje_asistencia = int(input("INGRESE EL PORCENTAJE DE ASISTENCIA DEL CURSADO: "))
+    if porcentaje_asistencia >= 50:
+        print("ASISTIO LA MAYORIA")
+    else:
+        print("NO ASISTIO LA MAYORIA")
+
+
 
