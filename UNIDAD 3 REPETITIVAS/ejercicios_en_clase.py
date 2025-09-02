b36672dff5be9380a02eea61faa2d53b8573c58e
juanmagarciat <garciajuanmartin258@gmail.com>
2025-09-01 23:12:58 -0300

    ejercicio 2 con while comprobado y verificado

--- UNIDAD 3 REPETITIVAS/ejercicios_en_clase.py
+++ UNIDAD 3 REPETITIVAS/ejercicios_en_clase.py
@@ -18,3 +18,84 @@ for i in range(5):
         encriptado = encriptado + letra_corrida  # JUNTA TODAS LAS LETRAS PARA MOSTRAR EL MENSAJE ENCRIPTADO
     print(encriptado)
     encriptado = ""
+
+#EJERCICIO 2 (WHILE)
+#JUEGO DE PIEDRA PAPEL O TIJERA CONTRA LA COMPUTADORA
+print("JUEGO DE PIEDRA PAPEL O TIJERA")
+opciones = {1: "PIEDRA", 2: "PAPEL", 3: "TIJERA"}
+print(opciones)
+juega = 1
+import random
+ganadas_usuario = 0
+ganadas_computadora = 0
+empates = 0
+while juega != 0:
+    try:
+        eleccion_usuario = int(input("LA OPCION ELEGIDA ES (1/2/3): "))
+    except ValueError:
+        print("INGRESE UNA OPCION VALIDA (1, 2 o 3)")
+        continue
+    if eleccion_usuario > 3 or eleccion_usuario < 1:
+        print("INGRESE UNA OPCION VALIDA (1, 2 o 3)")
+        continue
+    eleccion_computadora = random.randint(1, 3)
+    if eleccion_usuario == 1:
+        if eleccion_computadora == 3:
+            ganadas_usuario = ganadas_usuario + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("GANASTE!!")
+        elif eleccion_computadora == 2:
+            ganadas_computadora = ganadas_computadora + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("PERDISTE!!")
+        elif eleccion_computadora == 1:
+            empates = empates + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("EMPATE!!")
+    elif eleccion_usuario == 2:
+        if eleccion_computadora == 3:
+            ganadas_computadora = ganadas_computadora + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("PERDISTE!!")
+        elif eleccion_computadora == 2:
+            empates = empates + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("EMPATE!!")
+        elif eleccion_computadora == 1:
+            ganadas_usuario = ganadas_usuario + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("GANASTE!!")
+    elif eleccion_usuario == 3:
+        if eleccion_computadora == 3:
+            empates = empates + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("EMPATE!!")
+        elif eleccion_computadora == 2:
+            ganadas_usuario = ganadas_usuario + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("GANASTE!!")
+        elif eleccion_computadora == 1:
+            ganadas_computadora = ganadas_computadora + 1
+            print(f"USUARIO: {opciones[eleccion_usuario]}")
+            print(f"COMPUTADORA: {opciones[eleccion_computadora]}")
+            print("PERDISTE!!")
+    try:
+        juega = int(input("QUIERES JUGAR OTRA RONDA O SALIR DEL JUEGO [1: PARA CONTINUAR, 0: PARA SALIR]: "))
+    except ValueError:
+            print("ERROR (SALIENDO DEL JUEGO...)")
+            break
+    if juega not in (0, 1):
+        print("ERROR (SALIENDO DEL JUEGO...)")
+        break
+print("")
+print(f"GANADAS USUARIO: {ganadas_usuario}")
+print(f"GANADAS COMPUTADORA: {ganadas_computadora}")
+print(f"EMPATES: {empates}")
